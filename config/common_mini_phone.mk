011a93ca0580274fd85e4b44292b46e53f0b7d97
Konsta <konsta09@gmail.com>
2012-08-02 22:01:52 +0300

    set default ringtones, use old audio
    Change-Id: Ia4a6dff5dc46cfd02dd549f61aba3ba6be4c064d

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -2,19 +2,19 @@
 $(call inherit-product, vendor/cm/config/common.mk)
 
 # Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
+include frameworks/base/data/sounds/OldAudio.mk
 
 # Include CM audio files
 #include vendor/cm/config/cm_audio.mk
 
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=CyanTone.ogg \
-    ro.config.notification_sound=CyanMessage.ogg \
-    ro.config.alarm_alert=CyanAlarm.ogg
+    ro.config.ringtone=Ring_Digital_02.ogg \
+    ro.config.notification_sound=F1_New_SMS.ogg \
+    ro.config.alarm_alert=Alarm_Beep_03.ogg
 
 PRODUCT_PACKAGES += \
-  Mms
+    Mms
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
